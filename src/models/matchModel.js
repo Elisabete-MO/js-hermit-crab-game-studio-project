5d3af2092b609c4bfe4af9263d51a3434cdee8c1
Elisabete-MO <bete.mrt@gmail.com>
2025-07-27 21:17:02 -0300

    chore: add MATCH_STATES

--- src/models/matchModel.js
+++ src/models/matchModel.js
@@ -1,11 +1,18 @@
 const { v4: uuidv4 } = require('uuid');
 
+const MATCH_STATES = {
+  WAITING: 'waiting',
+  IN_PROGRESS: 'in_progress',
+  FINISHED: 'finished',
+  CANCELLED: 'cancelled',
+};
+
 class Match {
   constructor({ name }) {
     this.id = uuidv4();
     this.name = name;
     this.players = []; // máximo de 4 jogadores
-    this.state = 'waiting'; // waiting | in_progress | finished
+    this.state = MATCH_STATES.WAITING;
     this.startDate = null;
     this.scores = {}; // { playerId: score }
   }
