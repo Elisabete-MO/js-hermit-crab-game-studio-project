4c430c22386341e27667278ffac853a041b9dcc6
Elisabete-MO <bete.mrt@gmail.com>
2025-07-28 00:44:32 -0300

    feat: create Player service

--- src/services/matchService.js
+++ src/services/matchService.js
@@ -1,5 +1,5 @@
 const matches = require('../database/matchStorage');
-const Match = require('../models/Match');
+const Match = require('../models/matchModel');
 const MATCH_STATES = require('../constants/matchStates');
 const { v4: uuidv4 } = require('uuid');
 
